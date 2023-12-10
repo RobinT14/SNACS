102b1d1369e9093d76461fe61c44da75c82225fc
RT <robin.the@xs4all.nl>
2023-12-10 14:49:31 +0100

    Airport data

---
+++ Scripts/USairport500/networkit_script.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+import networkit as nk
+import time
+import json
+
+
+if __name__ == "__main__":
+    filename = '../../Data/USairport500.txt'
+    current_datetime = datetime.now()
+    current_date_string = current_datetime.strftime("%d-%m-%Y")
+
+    try:
+        G = nk.readGraph(
+            filename, nk.Format.EdgeListSpaceZero, directed=True, weighted=True)
+    except Exception as e:
+        print('Error:', e)
+        exit(1)
+
+    # "Geisberger" approach:
+    for i in range(0, 10):
+        start_time_geisberger = time.time()
+        betweenness = nk.centrality.EstimateBetweenness(G, 50, True, False)
+        betweenness.run()
+        end_time_geisberger = time.time()
+
+        # Write exact betweenness to file:
+        path = f"NetworKit_Results/{current_date_string}_networkit_geisberger_{str(i)}.json"
+        with open(path, 'w') as json_file:
+            json.dump(dict(betweenness.ranking()), json_file,
+                      indent=2, sort_keys=True)
+
+         # Output statistics
+        printLine = f'Approximation_Geisberger_NetworKit_{i},' + filename + ',' +\
+            str(end_time_geisberger - start_time_geisberger) + ',' + path + '\n'
+        with open('networkit_output.log', 'a') as file:
+            file.write(printLine)
+
+    # "Riondato" approach:
+    for i in range(0, 10):
+        start_time_riondato = time.time()
+        betweenness_riondato = nk.centrality.ApproxBetweenness(G,
+                                                               epsilon=0.1,
+                                                               delta=0.1,
+                                                               universalConstant=0.5)
+        betweenness_riondato.run()
+        end_time_riondato = time.time()
+
+        # Write exact betweenness to file:
+        path = f"NetworKit_Results/{current_date_string}_networkit_riondato_{str(i)}.json"
+        with open(path, 'w') as json_file:
+            json.dump(dict(betweenness_riondato.ranking()), json_file,
+                      indent=2, sort_keys=True)
+
+         # Output statistics
+        printLine = f'Approximation_Riondato_NetworKit_{i},' + filename + ',' +\
+            str(end_time_riondato - start_time_riondato) + ',' + path + '\n'
+        with open('networkit_output.log', 'a') as file:
+            file.write(printLine)
+
+    # Van der Grinten A., Angriman E., and Meyerhenke H. (2019) approach of Kadabra algorithm
+    for i in range(0, 10):
+        start_time_kadabra = time.time()
+        betweenness_kadabra = nk.centrality.KadabraBetweenness(
+            G, 0.05, 0.8)  # these are the default settings
+        betweenness_kadabra.run()
+        end_time_kadabra = time.time()
+
+        # Write exact betweenness to file:
+        path = f"NetworKit_Results/{current_date_string}_networkit_kadabra_{str(i)}.json"
+        with open(path, 'w') as json_file:
+            json.dump(dict(betweenness_kadabra.ranking()), json_file,
+                      indent=2, sort_keys=True)
+
+         # Output statistics
+        printLine = f'Approximation_Kadabra_NetworKit_{i},' + filename + ',' +\
+            str(end_time_kadabra - start_time_kadabra) + ',' + path + '\n'
+        with open('networkit_output.log', 'a') as file:
+            file.write(printLine)
+
+    # "Bergamini and Meyerhenke" approach:
+    for i in range(0, 10):
+        start_time_bergamini = time.time()
+        betweenness_bergamini = nk.centrality.DynApproxBetweenness(
+            G, epsilon=0.1, delta=0.1, storePredecessors=True, universalConstant=0.5)
+        betweenness_bergamini.run()
+        end_time_bergamini = time.time()
+
+        # Write exact betweenness to file:
+        path = f"NetworKit_Results/{current_date_string}_networkit_bergamini_{str(i)}.json"
+        with open(path, 'w') as json_file:
+            json.dump(dict(betweenness_bergamini.ranking()), json_file,
+                      indent=2, sort_keys=True)
+
+         # Output statistics
+        printLine = f'Approximation_Bergamini_NetworKit_{i},' + filename + ',' +\
+            str(end_time_bergamini - start_time_bergamini) + ',' + path + '\n'
+        with open('networkit_output.log', 'a') as file:
+            file.write(printLine)
---
+++ Scripts/USairport500/networkx_script.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+import networkx as nx
+import time
+import json
+import math
+
+# Undirected and unweighted:
+if __name__ == "__main__":
+
+    filename = '../../Data/USairport500.txt'
+    current_datetime = datetime.now()
+    current_date_string = current_datetime.strftime("%d-%m-%Y")
+
+    G = nx.read_edgelist(filename,
+                         nodetype=int,
+                         create_using=nx.DiGraph,
+                         data=(("weight", int), ))
+
+    start_time_exact = time.time()
+    betweenness = nx.betweenness_centrality(G)
+    end_time_exact = time.time()
+
+    # Write exact betweenness to file:
+    sorted_betweenness = {k: betweenness[k] for k in sorted(betweenness)}
+    path = f"NetworkX_Results/{current_date_string}_networkx_exact.json"
+    with open(path, 'w') as json_file:
+        json.dump(sorted_betweenness, json_file, indent=2, sort_keys=True)
+
+    # Output statistics
+    printLine = 'Exact_Brandes_NetworkX,' + filename + ',' +\
+        str(end_time_exact - start_time_exact) + ',' + path + '\n'
+    with open('networkx_output.log', 'a') as file:
+        file.write(printLine)
+
+    # Approximations of betweenness using sampling/pivotting
+    # Sample sizes ranging from 20-80%, 10 runs for each sample size
+    samples = [20, 40, 60, 80]
+    for sample in samples:
+        sampleSize = math.floor(len(G.nodes) * (sample/100))
+        for i in range(0, 10):
+            # Approximation of betweenness
+            start_time_approx = time.time()
+            betweenness_approx = nx.betweenness_centrality(G, k=sampleSize)
+            end_time_approx = time.time()
+
+            # Write approximated betweenness to file
+            sorted_betweenness_approx = {k: betweenness_approx[k]
+                                         for k in sorted(betweenness_approx)}
+            path = f"NetworkX_Results/{current_date_string}_networkx_approximation_{str(sample)}_{str(i)}.json"
+            with open(path, 'w') as json_file:
+                json.dump(sorted_betweenness_approx, json_file,
+                          indent=2, sort_keys=True)
+
+            printLine = f'Approximation_Brandes_NetworkX_{str(sample)}_{str(i)},' + filename + ',' +\
+                str(end_time_approx - start_time_approx) + ',' + path + '\n'
+            with open('networkx_output.log', 'a') as file:
+                file.write(printLine)
